5cc1474a9317560f7815e355dc080c8f37bb79a2
kurtisharms <emailkurtis@gmail.com>
2014-10-06 19:01:53 -0700

    Updated to basic read version

--- SerialRunner/Form1.Designer.cs
+++ SerialRunner/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SerialRunner
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,87 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.readDataTimer = new System.Windows.Forms.Timer(this.components);
+            this.dataReceivedListBox = new System.Windows.Forms.ListBox();
+            this.connectionButton = new System.Windows.Forms.Button();
+            this.portSelectionComboBox = new System.Windows.Forms.ComboBox();
+            this.saveDataFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // readDataTimer
+            // 
+            this.readDataTimer.Tick += new System.EventHandler(this.readDataTimer_Tick);
+            // 
+            // dataReceivedListBox
+            // 
+            this.dataReceivedListBox.AccessibleName = "";
+            this.dataReceivedListBox.FormattingEnabled = true;
+            this.dataReceivedListBox.ItemHeight = 16;
+            this.dataReceivedListBox.Location = new System.Drawing.Point(28, 76);
+            this.dataReceivedListBox.Name = "dataReceivedListBox";
+            this.dataReceivedListBox.Size = new System.Drawing.Size(225, 116);
+            this.dataReceivedListBox.TabIndex = 0;
+            // 
+            // connectionButton
+            // 
+            this.connectionButton.Location = new System.Drawing.Point(178, 31);
+            this.connectionButton.Name = "connectionButton";
+            this.connectionButton.Size = new System.Drawing.Size(75, 23);
+            this.connectionButton.TabIndex = 1;
+            this.connectionButton.Text = "Connect";
+            this.connectionButton.UseVisualStyleBackColor = true;
+            this.connectionButton.Click += new System.EventHandler(this.connectionButton_Click);
+            // 
+            // portSelectionComboBox
+            // 
+            this.portSelectionComboBox.FormattingEnabled = true;
+            this.portSelectionComboBox.Location = new System.Drawing.Point(28, 30);
+            this.portSelectionComboBox.Name = "portSelectionComboBox";
+            this.portSelectionComboBox.Size = new System.Drawing.Size(121, 24);
+            this.portSelectionComboBox.TabIndex = 2;
+            // 
+            // saveDataFileButton
+            // 
+            this.saveDataFileButton.Location = new System.Drawing.Point(73, 218);
+            this.saveDataFileButton.Name = "saveDataFileButton";
+            this.saveDataFileButton.Size = new System.Drawing.Size(75, 23);
+            this.saveDataFileButton.TabIndex = 3;
+            this.saveDataFileButton.Text = "Save to File";
+            this.saveDataFileButton.UseVisualStyleBackColor = true;
+            this.saveDataFileButton.Click += new System.EventHandler(this.saveDataFileButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.saveDataFileButton);
+            this.Controls.Add(this.portSelectionComboBox);
+            this.Controls.Add(this.connectionButton);
+            this.Controls.Add(this.dataReceivedListBox);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Timer readDataTimer;
+        private System.Windows.Forms.ListBox dataReceivedListBox;
+        private System.Windows.Forms.Button connectionButton;
+        private System.Windows.Forms.ComboBox portSelectionComboBox;
+        private System.Windows.Forms.Button saveDataFileButton;
     }
 }
 
--- SerialRunner/Form1.cs
+++ SerialRunner/Form1.cs
@@ -7,14 +7,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections.Concurrent;
 
 namespace SerialRunner
 {
-    public partial class Form1 : Form
+       
+    public partial class MainForm : Form
     {
-        public Form1()
+        ConcurrentQueue<int> dataQueue = new ConcurrentQueue<int>();
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            portSelectionComboBox.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            if (portSelectionComboBox.Items.Count > 0)
+                portSelectionComboBox.SelectedIndex = 0;
+
+        }
+
+
+
+
+
+
+        private void saveDataFileButton_Click(object sender, EventArgs e)
+        {
+            StreamWriter outputFile;
+            saveFileDialog1.InitialDirectory = Application.StartupPath;
+            saveFileDialog1.ShowDialog();
+            outputFile = new StreamWriter(saveFileDialog1.FileName);
+            //outputFile.WriteLine("X,Y,Z");
+            for (int i = 0; i < dataReceivedListBox.Items.Count; i++)
+            {
+                outputFile.WriteLine(dataReceivedListBox.Items[i]);
+
+            }
+
+            outputFile.Close();
+        }
+
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            while (serialPort.ReadBufferSize > 0)
+            {
+                dataQueue.Enqueue(serialPort.ReadByte());
+            }
+        }
+
+        private void connectionButton_Click(object sender, EventArgs e)
+        {
+             try
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.PortName = portSelectionComboBox.SelectedItem.ToString();
+                    serialPort.Open();
+                    //tinyStickSerialPort.DtrEnable = true;
+                    connectionButton.Text = "Disconnect";
+                    readDataTimer.Enabled = true;
+                    //serialPort1.WriteLine("LOG COM1 BESTPOSB ONTIME 1[CR]");
+                    serialPort.WriteLine("log com1 loglist");
+                }
+                else
+                {
+                    serialPort.Close();
+                    dataQueue = new ConcurrentQueue<int>();
+                    connectionButton.Text = "Connect";
+                    readDataTimer.Enabled = false;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                // Handles case of trying to connect to an empty COM port
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            readDataTimer.Stop();
+            if (serialPort.IsOpen)
+                serialPort.Close();
+        }
+
+        private void readDataTimer_Tick(object sender, EventArgs e)
+        {
+            int result;
+            if (dataQueue.TryDequeue(out result))
+            {
+                char c = Convert.ToChar(result);
+                string d = c.ToString();
+                dataReceivedListBox.Items.Add(d);
+
+            }
         }
     }
 }
